af772b7be0dc8bc5001d273ae2f9777980eae120
Matthew Carter <m@ahungry.com>
2019-10-04 21:08:55 -0400

    Port over the gui files

---
+++ Makefile
@@ -0,0 +1,31 @@
+default: help
+
+all: build
+
+test:
+	lein test
+
+# export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=lcd'
+run: target/uberjar/p99-0.1.0-SNAPSHOT-standalone.jar
+	java \
+		-Dswing.aatext=true \
+		-Dawt.useSystemAAFontSettings=lcd \
+		-jar ./target/uberjar/p99-0.1.0-SNAPSHOT-standalone.jar
+
+target/uberjar/p99-1.0.0-SNAPSHOT-standalone.jar:
+	lein deps
+	lein uberjar
+
+build:
+	lein uberjar
+
+uberjar: build
+
+list-deps:
+	lein deps :tree
+
+help:
+	$(info Hint: Run something like - make build && make test && make run)
+	$(info Available commands: [ all | test | run | build | list-deps | help ])
+
+.PHONY: uberjar build test help list-deps
--- project.clj
+++ project.clj
@@ -1,9 +1,43 @@
 (defproject ahungry/p99 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A set of project1999 utilities and features."
+  :url "https://github.com/ahungry/p99"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [
+                 [org.clojure/clojure "1.10.0"]
+                 [seesaw "1.5.0"]
+
+                 ;; Latest versions, but only work with java 9+
+                 ;; [org.pushing-pixels/radiance-substance "2.5.1"]
+                 [org.pushing-pixels/radiance-substance "1.0.2"]
+
+                 ;; Logging related
+                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [org.clojure/tools.logging "0.5.0"]
+
+                 ;; Network related
+                 [clj-http "3.10.0"]
+                 [cheshire "5.9.0"]
+
+                 ;; Config or persistence related
+                 [ahungry/xdg-rc "0.0.4"]
+                 ]
   :main ^:skip-aot ahungry.p99
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :repl-options {:init-ns ahungry.gui}
+  :java-source-paths ["java-src"]
+  :aot []
+  :profiles {
+             ;; Required to use clojure spec check facilities.
+             :dev {:dependencies [[org.clojure/test.check "0.9.0"]]}
+             :uberjar {:aot :all}
+             }
+  :jvm-opts ["-Dfile.encoding=UTF8"
+             "-Dswing.aatext=true"
+             "-Dawt.useSystemAAFontSettings=lcd"
+             ;; "-Dswing.defaultlaf=org.pushingpixels.substance.api.skin.DustSkin"
+             ]
+  :pom-addition [:properties
+                 ["maven.compiler.source" "1.8"]
+                 ["maven.compiler.target" "1.8"]]
+  )
