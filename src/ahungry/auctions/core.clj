d7a947041258a9ea1461d8cc831578c0b7bf010c
Matthew Carter <m@ahungry.com>
2019-10-25 00:37:30 -0400

    Add way to parse and send log file for aucs

---
+++ src/ahungry/auctions/core.clj
@@ -0,0 +1,28 @@
+(ns ahungry.auctions.core
+  (:require
+   [clj-http.client :as client]
+   [cheshire.core :as cheshire]
+   [clojure.tools.logging :as log]
+   [ahungry.fs.logs :as logs]
+   [ahungry.net :as net]))
+
+(defn green? [s]
+  (if (re-find #"(?i)green" s)
+    1
+    0))
+
+(defn get-auctions-for-player []
+  (let [{:keys [name server]} (logs/get-player-info)]
+    {:green (green? server)
+     :dump
+     (->> (logs/get-auctions name)
+          (clojure.string/join "\r\n"))}))
+
+(defn post-auctions
+  "Send the auctions to the server, hooray."
+  []
+  (client/post
+   "https://ahungry.com/aucDump.php"
+   ;; {:body (cheshire/encode (get-auctions-for-player))}
+   {:form-params (get-auctions-for-player)}
+   ))
