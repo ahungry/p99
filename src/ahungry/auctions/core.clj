8d671746d960c5b6a5be3ad05d0918a80b6d8bad
Matthew Carter <m@ahungry.com>
2019-11-13 23:39:34 -0500

    correctly post teal vs green etc.

--- src/ahungry/auctions/core.clj
+++ src/ahungry/auctions/core.clj
@@ -7,13 +7,19 @@
    [ahungry.net :as net]))
 
 (defn green? [s]
-  (if (re-find #"(?i)(green|teal)" s)
+  (if (re-find #"(?i)(green)" s)
+    1
+    0))
+
+(defn teal? [s]
+  (if (re-find #"(?i)(teal)" s)
     1
     0))
 
 (defn get-auctions-for-player []
   (let [{:keys [name server]} (logs/get-player-info)]
     {:green (green? server)
+     :teal (teal? server)
      :dump
      (->> (logs/get-auctions name)
           (clojure.string/join "\r\n"))}))
