cffa87b25ea57440061b98fce3c8f9493c1c15c7
Matthew Carter <m@ahungry.com>
2020-01-14 00:00:38 -0500

    Drop the sleep down

--- src/ahungry/gui.clj
+++ src/ahungry/gui.clj
@@ -58,7 +58,7 @@
 (def ^:dynamic *auction-loop* true)
 (def ^:dynamic *auction-delay* 30000)
 (def ^:dynamic *redraw-loop* true)
-(def ^:dynamic *sleep-delay* 3000)
+(def ^:dynamic *sleep-delay* 500)
 
 (defn auction-loop []
   (when (:auction-loop @*state)
